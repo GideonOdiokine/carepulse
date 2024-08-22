209466c0721082cb539bb42bfcd89ad0cab42fb9
GideonOdiokine <gidysmartodiokineh@gmail.com>
2024-08-22 02:43:43 +0100

    tailwindcss setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <div>
       <h1 className='text-3x underline'>Home</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
