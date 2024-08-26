f0c08d0cf1512b188a26d106957e3bdee4ec442c
GideonOdiokine <gidysmartodiokineh@gmail.com>
2024-08-26 05:21:27 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <meta  name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"></meta>
       <body
         className={cn(
           "min-h-screen bg-dark-300 font-mont antialiased",
